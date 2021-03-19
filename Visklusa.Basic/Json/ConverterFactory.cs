fb4efc5ecb4d98cb8875d7d03713bca0ff9f2095
NumAniCloud <num.ani.cloud@gmail.com>
2021-03-19 14:52:22 +0900

    プリセットとなるプロジェクトを追加

---
+++ Visklusa.Basic/Json/ConverterFactory.cs
@@ -0,0 +1,19 @@
+﻿using Visklusa.Notation.Json;
+
+namespace Visklusa.Basic.Json
+{
+	public class ConverterFactory
+	{
+		public IJsonCapabilityConverter Transform2Converter() =>
+			new JsonCapabilityBase<Transform2>(Transform2.Id);
+
+		public IJsonCapabilityConverter TextureConverter() =>
+			new JsonCapabilityBase<Texture>(Texture.Id);
+
+		public IJsonCapabilityConverter ColorDataConverter() =>
+			new JsonCapabilityBase<ColorData>(ColorData.Id);
+
+		public IJsonCapabilityConverter RoundedRectangleConverter() =>
+			new JsonCapabilityBase<RoundedRectangle>(RoundedRectangle.Id);
+	}
+}
---
+++ Visklusa.Basic/Records.cs
@@ -0,0 +1,35 @@
+﻿using System.Drawing;
+using System.Numerics;
+using Visklusa.Notation;
+
+namespace Visklusa.Basic
+{
+	public record Transform2(Vector2 Position, Vector2 Scale, float Angle) : ICapability
+	{
+		public static readonly string Id = "Visklusa.Basic.Transform2";
+
+		public string CapabilityId { get; } = Id;
+	}
+
+	public record Texture(string FilePath) : ICapability
+	{
+		public static readonly string Id = "Visklusa.Basic.Texture";
+
+		public string CapabilityId { get; } = Id;
+	}
+
+	public record ColorData(Color Color) : ICapability
+	{
+		public static readonly string Id = "Visklusa.Basic.ColorData";
+
+		public string CapabilityId { get; } = Id;
+	}
+
+	public record RoundedRectangle
+		(float TopLeft, float TopRight, float BottomLeft, float BottomRight) : ICapability
+	{
+		public static readonly string Id = "Visklusa.Basic.RoundedRectangle";
+
+		public string CapabilityId { get; } = Id;
+	}
+}
