89724f2b4516fbc6a44fdfbfa005dc126c27d760
NumAniCloud <num.ani.cloud@gmail.com>
2021-03-26 14:46:54 +0900

    Visklusa.JsonZipのリリースフローを整備

--- Workflow/publish-local.ps1
+++ Workflow/publish-local.ps1
@@ -5,4 +5,4 @@ $project = "..\Dev\${projectName}\${projectName}.csproj"
 $version = nbgv get-version -p $project -v NuGetPackageVersion
 dotnet pack $project -o Pack/ -v minimal -p:PackageVersion=$version
 
-dotnet nuget push "Pack/${projectName}.${version}.nupkg" -s D:\Home\MyDocuments\Projects\MyNugetFeed\${projectName}
+dotnet nuget push "Pack/${projectName}.${version}.nupkg" -s D:\Home\MyDocuments\Projects\MyNugetFeed\Visklusa
