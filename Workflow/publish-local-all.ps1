5ac727d9e978e7551124a674ca52c615b122e309
NumAniCloud <num.ani.cloud@gmail.com>
2022-12-04 12:38:44 +0900

    不要なファイルを削除

--- Workflow/publish-local-all.ps1
+++
@@ -1,6 +0,0 @@
-./publish-local.ps1 Visklusa.Abstraction
-./publish-local.ps1 Visklusa.Archiver.Zip
-./publish-local.ps1 Visklusa.IO
-./publish-local.ps1 Visklusa.JsonZip
-./publish-local.ps1 Visklusa.Notation.Json
-./publish-local.ps1 Visklusa.Preset
