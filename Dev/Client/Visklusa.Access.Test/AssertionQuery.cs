bd66cf1a676354fa30df4db9acd4b4adc8eea31e
NumAniCloud <num.ani.cloud@gmail.com>
2022-12-04 16:52:49 +0900

    テストを追加

--- Dev/Client/Visklusa.Access.Test/AssertionQuery.cs
+++ Dev/Client/Visklusa.Access.Test/AssertionQuery.cs
@@ -2,25 +2,25 @@
 
 namespace Visklusa.Access.Test;
 
-internal class AssertionContext<T>
+internal class AssertionQuery<T>
 {
     public required T Subject { private get; init; }
 
-    public AssertionContext<T> AssertThat<TActual>(Func<T, TActual> selector, Constraint constraint)
+    public AssertionQuery<T> AssertThat<TActual>(Func<T, TActual> selector, Constraint constraint)
     {
         Assert.That(selector(Subject), constraint);
         return this;
     }
 
-    public AssertionContext<T> OnSequence<TActual>(Func<T, IEnumerable<TActual>> selector,
-        params Action<AssertionContext<TActual>>[] childAssertions)
+    public AssertionQuery<T> OnSequence<TActual>(Func<T, IEnumerable<TActual>> selector,
+        params Action<AssertionQuery<TActual>>[] childAssertions)
     {
         var array = selector(Subject).ToArray();
         Assert.That(array.Length, Is.EqualTo(childAssertions.Length));
 
         for (int i = 0; i < array.Length; i++)
         {
-            childAssertions[i].Invoke(new AssertionContext<TActual>()
+            childAssertions[i].Invoke(new AssertionQuery<TActual>()
             {
                 Subject = array[i]
             });
@@ -30,11 +30,11 @@ internal class AssertionContext<T>
     }
 }
 
-internal static class AssertionContext
+internal static class AssertionQuery
 {
-    public static AssertionContext<T> OnObject<T>(T subject)
+    public static AssertionQuery<T> OnObject<T>(T subject)
     {
-        return new AssertionContext<T>()
+        return new AssertionQuery<T>()
         {
             Subject = subject
         };
--- Dev/Client/Visklusa.Access.Test/ParentInfoLoaderTest.cs
+++ Dev/Client/Visklusa.Access.Test/ParentInfoLoaderTest.cs
@@ -23,7 +23,7 @@ public class ParentInfoLoaderTest
         Assert.That(actual.Length, Is.EqualTo(1));
         Assert.That(actual[0].Self, Is.EqualTo(layout.Elements[0]));
 
-        AssertionContext.OnObject(actual)
+        AssertionQuery.OnObject(actual)
             .OnSequence(x => x,
                 l0 => l0.AssertThat(x => x.Self, Is.EqualTo(layout.Elements[0]))
                     .OnSequence(x => x.Children));
@@ -42,7 +42,7 @@ public class ParentInfoLoaderTest
 
         var actual = loader.GetElementRoots().ToArray();
 
-        AssertionContext.OnObject(actual)
+        AssertionQuery.OnObject(actual)
             .OnSequence(x => x,
                 l0 => l0.AssertThat(x => x.Self, Is.EqualTo(layout.Elements[0]))
                     .OnSequence(x => x.Children,
@@ -66,7 +66,7 @@ public class ParentInfoLoaderTest
 
         var actual = loader.GetElementRoots().ToArray();
 
-        AssertionContext.OnObject(actual)
+        AssertionQuery.OnObject(actual)
             .OnSequence(x => x,
                 l0 => l0.AssertThat(x => x.Self, Is.EqualTo(layout.Elements[0]))
                     .OnSequence(x => x.Children,
@@ -74,5 +74,62 @@ public class ParentInfoLoaderTest
                             .OnSequence(x => x.Children,
                                 l2 => l2.AssertThat(x => x.Self, Is.EqualTo(layout.Elements[2]))
                                     .OnSequence(x => x.Children))));
+    }
+
+    [Test]
+    public void ノードが0個のときは空になる()
+    {
+        var layout = new Layout(
+            new CapabilityAssertion(FamilyShip.IdToRead));
+        var loader = new ParentInfoLoader(layout);
+
+        var actual = loader.GetElementRoots().ToArray();
+
+        AssertionQuery.OnObject(actual).OnSequence(x => x);
+    }
+
+    [Test]
+    public void 二分木状の7個のノードを解析できる()
+    {
+        var layout = new Layout(
+            new CapabilityAssertion(FamilyShip.IdToRead),
+            new Element(0),
+            new Element(1, new FamilyShip(0)),
+            new Element(2, new FamilyShip(0)),
+            new Element(3, new FamilyShip(1)),
+            new Element(4, new FamilyShip(1)),
+            new Element(5, new FamilyShip(2)),
+            new Element(6, new FamilyShip(2)));
+        var loader = new ParentInfoLoader(layout);
+
+        var actual = loader.GetElementRoots().ToArray();
+
+        AssertionQuery.OnObject(actual)
+            .OnSequence(x => x,
+                l0 => layout.AssertElement(l0, 0)
+                    .OnSequence(x => x.Children,
+                        l1 => layout.AssertElement(l1, 1)
+                            .OnSequence(x => x.Children,
+                                l2 => layout.AssertElement(l2, 3)
+                                    .OnSequence(x => x.Children),
+                                l2 => layout.AssertElement(l2, 4)
+                                    .OnSequence(x => x.Children)),
+                        l1 => layout.AssertElement(l1, 2)
+                            .OnSequence(x => x.Children,
+                                l2 => layout.AssertElement(l2, 5)
+                                    .OnSequence(x => x.Children),
+                                l2 => layout.AssertElement(l2, 6)
+                                    .OnSequence(x => x.Children))));
+    }
+}
+
+internal static class ElementTestHelper
+{
+    public static AssertionQuery<ElementFamily> AssertElement(
+        this Layout layout,
+        AssertionQuery<ElementFamily> node,
+        int index)
+    {
+        return node.AssertThat(x => x.Self, Is.EqualTo(layout.Elements[index]));
     }
 }
